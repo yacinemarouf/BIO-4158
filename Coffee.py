f797ab182240ef42e8f6ade5294e5a0d2958ec87
kpham041 <109395968+kpham041@users.noreply.github.com>
2022-09-19 16:26:17 -0400

    yes, that's coffee

---
+++ Coffee.py
@@ -0,0 +1,12 @@
+
+
+
+name = input("what is ur name ? ")
+
+password = input ("What's ur password ? ")
+
+password_strong= len(password)
+
+star = password_strong * "*"
+
+print( " Welcome Isabella + star )
